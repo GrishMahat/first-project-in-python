e9fccefa6c954127248dde86335b44fd2b013aff
Grish Mahat <114584253+GrishMahat@users.noreply.github.com>
2022-10-17 09:34:06 +0545

    Add files via upload
    rock paper scissors

---
+++ test2.py
@@ -0,0 +1,33 @@
+import random
+
+while True:
+    list = ["rock", "paper", "scissors"]
+    bot = random.choice(list)
+    print(bot)
+    player = str(input(f"enter  {list}\n"))
+    
+    if bot == player:
+        print("draw")
+    
+    elif player == "rock":
+        if bot == "scissors":
+            print("you win")
+        elif bot == "paper":
+            print("you lost")
+               
+    elif player == "paper":
+        if bot == "scissors":
+            print("you win")
+        elif bot == "rock":
+            print("you lost")
+        
+    elif player == "scissors":
+        if bot == "paper":
+            print("you lost")
+        if bot == "rock":
+            print("you win")    
+    
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
+    
